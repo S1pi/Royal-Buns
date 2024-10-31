97bbf684ab0921ccca08b37cd9f9aa9ef006eeaf
Miika Sipilä <miikavsipila@gmail.com>
2024-11-01 00:14:50 +0200

    Some testing files

---
+++ src/components/testikomponentti/test1.ts
@@ -0,0 +1,9 @@
+const testContent = document.querySelector('.testContent');
+
+const h2 = document.createElement('h2');
+
+h2.textContent = 'HIENO HEADER2';
+
+h2.classList.add('mt-2');
+
+testContent?.appendChild(h2);
