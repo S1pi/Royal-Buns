67c201f798dc6c6148c123857379050425f00224
S1pi <miikavsipila@gmail.com>
2024-11-05 03:03:44 +0200

    Modiefied header

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,18 +1,34 @@
 const header = (element: HTMLElement) => {
-  // const body = document.querySelector('body');
+  const body = document.querySelector('body') as HTMLBodyElement;
   const header = document.createElement('header');
+
   // Annetaan tailwind css luokkia
   header.classList.add('bg-red-75', 'h-16');
+  header.classList.add('flex');
 
   const nav = document.createElement('nav');
+  nav.classList.add('bg-black');
+
+  const headerLinks = document.createElement('ul');
+
+  const links = [
+    {name: 'Etusivu', value: 'etusivu'},
+    {name: 'Menu', value: 'menu'},
+  ];
+
   const headerText = document.createElement('h1');
+
+  const rbLogo = document.createElement('img');
+  rbLogo.src = '/img/rb-logo.png';
+  rbLogo.alt = 'Royalbuns Logo';
+
+  header.append(rbLogo);
 
   headerText.textContent = 'Kiva header';
   nav.appendChild(headerText);
 
   header.appendChild(nav);
-  // body?.appendChild(header);
-  element.appendChild(header);
+  body.appendChild(header);
 };
 
 const navigation = (page: string) => {
