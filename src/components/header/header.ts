6313814352ded2fc41e6044a3be9c5c43d2915e6
S1pi <miikavsipila@gmail.com>
2024-11-05 04:05:58 +0200

    Created header basis

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -3,31 +3,83 @@ const header = (element: HTMLElement) => {
   const header = document.createElement('header');
 
   // Annetaan tailwind css luokkia
-  header.classList.add('bg-red-75', 'h-16');
+  header.classList.add('bg-secondary', 'h-16');
   header.classList.add('flex');
 
   const nav = document.createElement('nav');
-  nav.classList.add('bg-black');
+  nav.classList.add('bg-primary');
 
   const headerLinks = document.createElement('ul');
+  headerLinks.classList.add('flex', 'gap-8', 'h-full', 'items-center', 'px-4');
 
   const links = [
-    {name: 'Etusivu', value: 'etusivu'},
+    {name: 'Etusivu', value: 'main'},
     {name: 'Menu', value: 'menu'},
+    {name: 'Varaa Pöytä', value: 'reservation'},
+    {name: 'Ravintolat', value: 'restaurants'},
+    {name: 'Galleria', value: 'gallery'},
+    {name: 'Yrityksille', value: 'business'},
   ];
 
-  const headerText = document.createElement('h1');
+  links.forEach((item) => {
+    const listItem = document.createElement('li') as HTMLLIElement;
+    listItem.className = 'hover:cursor-pointer h-3/4 flex items-center';
+    listItem.textContent = item.name;
+    listItem.dataset.value = item.value;
+    headerLinks.appendChild(listItem);
+  });
 
-  const rbLogo = document.createElement('img');
-  rbLogo.src = '/img/rb-logo.png';
-  rbLogo.alt = 'Royalbuns Logo';
+  nav.appendChild(headerLinks);
 
-  header.append(rbLogo);
+  const rbLogo = document.createElement('div');
+  rbLogo.classList.add('w-20');
+  rbLogo.style.backgroundImage = 'url("/img/rb-logo.png")';
+  rbLogo.style.backgroundSize = 'contain';
+  rbLogo.style.backgroundPosition = 'center';
+  rbLogo.style.backgroundRepeat = 'no-repeat';
+  const divSeperator = document.createElement('div');
+  divSeperator.classList.add(
+    'bg-primary',
+    'w-1',
+    'h-5/6',
+    'self-center',
+    'mx-6'
+  );
 
-  headerText.textContent = 'Kiva header';
-  nav.appendChild(headerText);
+  const logoContainer = document.createElement('div');
+  logoContainer.classList.add('flex');
+  const logos = [
+    {
+      logoName: 'Facebook',
+      logoSrc: '/img/placeholder.png',
+      href: 'https://www.facebook.com',
+    },
+    {
+      logoName: 'instagram',
+      logoSrc: '/img/placeholder.png',
+      href: 'https://www.instagram.com',
+    },
+    {
+      logoName: 'logIn',
+      logoSrc: '/img/placeholder.png',
+      href: '/login',
+    },
+  ];
 
-  header.appendChild(nav);
+  logos.forEach((item) => {
+    const link = document.createElement('a');
+    link.href = item.href;
+    link.className = 'flex mx-4';
+    const img = document.createElement('img');
+    img.src = item.logoSrc;
+    img.alt = item.logoName;
+    link.appendChild(img);
+    logoContainer.appendChild(link);
+  });
+
+  const languageContainer = document.createElement('div');
+
+  header.append(rbLogo, nav, divSeperator, logoContainer);
   body.appendChild(header);
 };
 
