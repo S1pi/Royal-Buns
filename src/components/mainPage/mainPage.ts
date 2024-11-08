cc895deff3fd0e9518e39e26b25fca2d81245da0
waltteriwe <walttewe@metropolia.fi>
2024-11-08 14:36:42 +0200

    working on reservation page

--- src/components/mainPage/mainPage.ts
+++ src/components/mainPage/mainPage.ts
@@ -1,8 +1,10 @@
 import {header} from '../header/header';
+import {infoPage} from '../infoPage/infoPage';
 
 const mainPage = () => {
   // First create header for page
   header(document.querySelector('body') as HTMLElement);
+  infoPage();
 };
 
 export {mainPage};
