f9ea26d312facafd370f58f5910efd2ac8982521
S1pi <miikavsipila@gmail.com>
2024-11-12 11:19:59 +0200

    Deleted duplicate headers

--- frontend/src/components/mainPage/mainPage.ts
+++ frontend/src/components/mainPage/mainPage.ts
@@ -1,10 +1,8 @@
 import {header} from '../header/header';
-import {infoPage} from '../infoPage/infoPage';
 
 const mainPage = () => {
   // First create header for page
   header();
-  infoPage();
 };
 
 export {mainPage};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,6 @@
-import {infoPage} from './components/infoPage/infoPage';
-import {mainPage} from './components/mainPage/mainPage';
 import {reservation} from './components/reservation/reservation';
 import {reservationModal} from './components/reservation/reservationModal';
 
-mainPage();
 document.addEventListener('DOMContentLoaded', () => {
   reservationModal();
 });
