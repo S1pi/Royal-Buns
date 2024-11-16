345b219360e7d844708a71f3c2f5507b88ad14d0
Jim Pohjansaro <jim.pohjansaro@gmail.com>
2024-11-16 20:23:04 +0200

    Created: forBusiness page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import {mainPage} from './components/mainPage/mainPage';
-import {resp} from './components/resp/resp';
+import {business} from './components/business/business';
+// import {mainPage} from './components/mainPage/mainPage';
+// import {resp} from './components/resp/resp';
 
-resp();
+business();
