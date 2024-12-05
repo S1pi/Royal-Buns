3f19f5e53b5fea22e2fdc8551191905ff6c2c679
S1pi <miikavsipila@gmail.com>
2024-12-05 18:56:18 +0200

    Code cleaning

--- backend/src/routes/user-router.ts
+++ backend/src/routes/user-router.ts
@@ -7,10 +7,6 @@ const userRouter = express.Router();
 // Käyttäjän tiedot sekä poisto
 // userRouter.route('/:id').get(getUser).put(putUser).delete(deleteUser);
 
-// Käyttäjän kirjautuminen /api/user/auth/login
-// Siirrä kokonaan auth-router:iin ja auth-controller etc.
-// userRouter.route('/auth/login').post(userLogin);
-
 // Hankkii tokenin avulla tiedot itsestään
 userRouter.route('/me').get(tokenAuth, getMe);
 
